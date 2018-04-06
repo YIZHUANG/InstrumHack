1ebe8c3bd6efce6d0b625a3dcaa6c29eadcf4593
YIZHUANG <yi.zhuang1@outlook.com>
2018-04-06 16:07:03 +0300

    initial reward system

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Switch, BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
+import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
+
 import Header from "./component/header";
 import ReactTable from "react-table";
-import Home from './component/home';
+import Home from "./component/home";
+import RewardSystem from "./component/RewardSystem";
 
 class App extends Component {
   render() {
     return (
-      <div>
-      <Header/>
-      <Home />
-      </div>
+      <MuiThemeProvider>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Switch>
+              <Route exact path="/RewardSystem" component={RewardSystem} />
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/component/table.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from "material-ui/Table";
+import RaisedButton from "material-ui/RaisedButton";
+
+const styles = {
+  whiteSpace: "normal",
+  wordWrap: "break-word"
+};
+
+const Tables = ({ job, key }) => {
+  return (
+    <Table key={key} className="table" style={{ width: "70%" }}>
+      <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+        <TableRow>
+          <TableHeaderColumn>description</TableHeaderColumn>
+          <TableHeaderColumn>industry</TableHeaderColumn>
+        </TableRow>
+      </TableHeader>
+      <TableBody displayRowCheckbox={false}>
+        <TableRow>
+          <TableRowColumn className="api_col" style={styles}>
+            {job.description}
+          </TableRowColumn>
+          <TableRowColumn style={styles}>{job.description}</TableRowColumn>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+};
+
+export default Tables;
