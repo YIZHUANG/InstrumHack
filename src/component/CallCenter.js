8742be130ab3143a232606097fb7339c1f43711f
YIZHUANG <yi.zhuang1@outlook.com>
2018-04-12 00:52:19 +0300

    added call

--- src/component/CallCenter.js
+++ src/component/CallCenter.js
@@ -1,53 +1,74 @@
-import React, { Component } from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-import CareJobs from './careJobs';
-import recognizeMic from 'watson-speech/speech-to-text/recognize-microphone';
+import React, { Component } from "react";
+import RaisedButton from "material-ui/RaisedButton";
+import CareJobs from "./careJobs";
+import recognizeMic from "watson-speech/speech-to-text/recognize-microphone";
+import axios from "axios";
+import ReactAudioPlayer from "react-audio-player";
 
 class CallCenter extends Component {
   constructor() {
-    super()
-    this.state = {}
+    super();
+    this.state = {};
   }
   onListenClick() {
-
-    fetch('http://localhost:3002/api/speech-to-text/token')
+    fetch("http://localhost:3002/api/speech-to-text/token")
       .then(function(response) {
-          return response.text();
-      }).then((token) => {
-        console.log('token is', token)
+        return response.text();
+      })
+      .then(token => {
         var stream = recognizeMic({
-            token: token,
-            objectMode: true, // send objects instead of text
-            extractResults: true, // convert {results: [{alternatives:[...]}], result_index: 0} to {alternatives: [...], index: 0}
-            format: false // optional - performs basic formatting on the results such as capitals an periods
+          token: token,
+          objectMode: true, // send objects instead of text
+          extractResults: true, // convert {results: [{alternatives:[...]}], result_index: 0} to {alternatives: [...], index: 0}
+          format: false // optional - performs basic formatting on the results such as capitals an periods
         });
-        stream.on('data', (data) => {
+        stream.on("data", data => {
           this.setState({
             text: data.alternatives[0].transcript
-          })
+          });
         });
-        stream.on('error', function(err) {
-            console.log(err);
+        stream.on("error", function(err) {
+          console.log(err);
         });
-        document.querySelector('#stop').onclick = stream.stop.bind(stream);
-      }).catch(function(error) {
-          console.log(error);
+        document.querySelector("#stop").onclick = stream.stop.bind(stream);
+      })
+      .catch(function(error) {
+        console.log(error);
       });
   }
+
+  onSend() {
+    return axios
+      .post("http://localhost:3002/api/text_to_speech", {
+        text: this.state.text
+      })
+      .then(() => console.log("yeah!"));
+  }
+
   render() {
+    console.log("text is", this.state.text);
     return (
       <div className="Container">
-      <div className="main-header-line">
-      <div className="container">
-      <h1>Call Center</h1>
-      <p>Speak to us about your inquiries</p>
-      </div>
-      </div>
-      <div className="App">
-        <RaisedButton style={{marginTop:50}} onClick={this.onListenClick.bind(this)} label="Click to speak" />
-        <div style={{fontSize: '40px'}}>{this.state.text}</div>
-      </div>
-      <CareJobs />
+        <div className="main-header-line">
+          <div className="container">
+            <h1>Call Center</h1>
+            <p>Speak to us about your inquiries</p>
+          </div>
+        </div>
+        <div className="App">
+          <RaisedButton
+            style={{ marginTop: 50 }}
+            onClick={this.onListenClick.bind(this)}
+            label="Click to speak"
+          />
+          <RaisedButton
+            style={{ marginTop: 50 }}
+            onClick={this.onSend.bind(this)}
+            label="Send"
+          />
+          <div style={{ fontSize: "40px" }}>{this.state.text}</div>
+        </div>
+        <CareJobs />
       </div>
     );
   }
